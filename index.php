0164da3d2f5e21f5b2ff7f86ee75e47041edbe73
6dario11 <dario.scherrer@sbw-media.ch>
2026-02-27 09:28:54 +0100

    Fix title formatting in index.php

--- index.php
+++ index.php
@@ -10,7 +10,7 @@ require_once './config.php';
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <link rel="stylesheet" href="./assets/styles/main.css">
-  <title> Modul 307</title>
+  <title>Modul 307</title>
 </head>
 <body>
   <h1>m307</h1>
